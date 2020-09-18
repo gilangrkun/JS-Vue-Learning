52d5d81b484e98b605797a58409541b956772bb5
kuntiarso <gilang.grk17@gmail.com>
2020-09-18 09:57:59 +0700

    declarative and directive rendering | Vue.js

---
+++ CDN/app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: "#vue-app",
+  data: {
+    message: "<i>Hello Indonesia</i>",
+    name: "<u><b>Alexandra</u></b>",
+  },
+  //   method: {
+  //     greet: function () {
+  //       return "Good morning";
+  //     },
+  //   },
+});
