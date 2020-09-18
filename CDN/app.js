5bd3a6ca3a7221c09b7d9e2ed219fd43d3552c56
kuntiarso <gilang.grk17@gmail.com>
2020-09-18 10:25:04 +0700

    add a method in Vue.js

--- CDN/app.js
+++ CDN/app.js
@@ -6,10 +6,13 @@ new Vue({
     m2: "Hai, kamu!",
     name: "Alexandra",
     show: false,
+    one: 1,
+    two: 2,
+    result: null,
   },
-  //   method: {
-  //     greet: function () {
-  //       return "Good morning";
-  //     },
-  //   },
+  methods: {
+    sum: function (newNumber) {
+      return (this.result = this.one + this.two + newNumber);
+    },
+  },
 });
